a96c1a3c6d0c13d2898d5f3b1088dbd6dbb439b6
AMAN KUMAR <ak6823700@gmail.com>
2025-12-02 16:47:13 +0530

    Add files via upload

---
+++ verify.py
@@ -0,0 +1,6 @@
+import langchain
+import langgraph
+from importlib.metadata import version
+
+print("✓ LangChain version:", version("langchain"))
+print("✓ LangGraph version:", version("langgraph"))
